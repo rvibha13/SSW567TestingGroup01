936d424b5bc59f2c63219a03d5f60e66fee8e893
Alexis Moore <alkamoore@gmail.com>
2016-09-09 16:17:35 -0400

    Merged with Sapana's (offline) code

--- src/Assignment01.java
+++ src/Assignment01.java
@@ -8,39 +8,93 @@
 *  1. classify triangle based on type       *
 *                                           *
 *********************************************/
+import java.io.*;
+
 
 public class Assignment01 {
-	//Program determines the type of triangle based on its parameters
+//	public static void main (String[] args) throws IOException
+//	  { 
+//		BufferedReader stdin =  new BufferedReader ( new InputStreamReader( System.in ) );
+//
+//	    String side1, side2, side3, end;
+//	    
+//	    boolean restart = false;
+//	    
+//	    int    a, b, c;                
+//	    
+//	    do {
+//	    System.out.println("Enter length of side1:");
+//	    side1   = stdin.readLine();
+//	    a   = Integer.parseInt(side1);          
+//
+//	    System.out.println("Enter length of side2:");
+//	    side2   = stdin.readLine();
+//	    b  = Integer.parseInt( side2 );          
+//
+//
+//	    System.out.println("Enter length of side3:");
+//	    side3   = stdin.readLine();
+//	    c  = Integer.parseInt( side3 );          
+//
+//	    System.out.println(classifyTriangle(a, b, c));
+//	    
+//	    System.out.println("Do you want to restart (Y or N)?:");
+//	    end = stdin.readLine();
+//	    if (end.equalsIgnoreCase("Y"))
+//	    {	
+//	    	restart = true;
+//	    }
+//	    else
+//	    {
+//	    	restart = false;
+//	    }
+//	    
+//	  }while (restart) ;
+//	    System.out.println("End of program!");
+//	  }
+//	
 	
-	public static int classifyTriangle(double a, double b, double c)
+	//Program determines the type of triangle based on its parameters
+	public static String classifyTriangle(double a, double b, double c)
 	{
-		//Triangle Type Definitions
-		// 0 EQUILATERAL
-		// 1 ISOSCELES
-		// 2 SCALENE
-		// 3 RIGHT
-		int triangleType = -1;
+		String triangle_type = "Invalid Triangle";
 		
-		//Determine if Equilateral Triangle
-		if(a == b && b == c)
+		//Checks that triangle is valid
+		if(a > 0 && b > 0 && c > 0)
 		{
-			triangleType = 0;
+			//Determine if Equilateral Triangle
+			if(a == b && b == c)
+			{
+				triangle_type = "Equilateral Triangle";
+			}
+			//Determine if Isosceles Triangle
+			else if(a == b || a == c || b == c)
+			{
+				//Determine if right triangle
+				if(((a * a) + (b * b)) == (c * c))
+				{
+					triangle_type = "Isosceles Right Triangle";
+				}
+				else
+				{
+					triangle_type = "Isosceles Triangle";
+				}
+			}
+			//Determine if Scalene Triangle
+			else
+			{
+				if(((a * a) + (b * b)) == (c * c)  //Note: doesn't use Math.pow because that function tends to decrease precision
+				|| ((a * a) + (c * c)) == (b * b)
+				|| ((b * b) + (c * c)) == (a * a)) 
+				{
+					triangle_type = "Scalene Right Triangle";
+				}
+				else
+				{
+					triangle_type = "Scalene Triangle";
+				}
+			}
 		}
-		//Determine if Right Triangle
-		else if (((a * a) + (b * b)) == (c * c))
-		{
-			triangleType = 3;
-		}
-		//Determine if Isosceles Triangle
-		else if(a == b || a == c || b == c)
-		{
-			triangleType = 1;
-		}
-		//Determine if Scalene Triangle
-		else
-		{
-			triangleType = 2;
-		}
-		return triangleType;
+		return triangle_type;
 	}
 }
--- src/Assignment01Junit.java
+++ src/Assignment01Junit.java
@@ -14,47 +14,113 @@ import static org.junit.Assert.assertEquals;
 public class Assignment01Junit {
 	
    @Test
-	//Checks if triangle is Equilateral for Int Inputs
+	//Checks if triangle is Equilateral for Integer Inputs
 	public void checkEquilateralI()
 	{
 		int a = 3;
 		int b = 3;
 		int c = 3;
-		int result = Assignment01.classifyTriangle(a, b, c);
-		assertEquals(result, 0);
+		String result = Assignment01.classifyTriangle(a, b, c);
+		assertEquals(result, "Equilateral Triangle");
 	}
 
    @Test
- //Checks if triangle is Equilateral for Double Inputs
+   //Checks if triangle is Equilateral for Double Inputs
  	public void checkEquilateralD()
  	{
  		double a = 3.53;
  		double b = 3.53;
  		double c = 3.53;
- 		int result = Assignment01.classifyTriangle(a, b, c);
- 		assertEquals(result, 0);
+ 		String result = Assignment01.classifyTriangle(a, b, c);
+ 		assertEquals(result, "Equilateral Triangle");
  	}
    
    @Test
-	//Checks if triangle is Right for Int Inputs
-	public void checkRightI()
+	//Checks if triangle is Scalene and Right for Integer Inputs
+	public void checkRRightScaleneInt()
 	{
 		int a = 3;
 		int b = 4;
 		int c = 5;
-		int result = Assignment01.classifyTriangle(a, b, c);
-		assertEquals(result, 3);
+		String result = Assignment01.classifyTriangle(a, b, c);
+		assertEquals(result, "Scalene Right Triangle");
 	}
 
    @Test
- //Checks if triangle is Right for Double Inputs
- 	public void checkRightD()
+   //Checks if triangle is Scalene and Right for Double Inputs
+ 	public void checkRightScaleneDouble()
  	{
  		double a = 5.0;
  		double b = 4.0;
  		double c = Math.sqrt(41);
- 		int result = Assignment01.classifyTriangle(a, b, c);
- 		assertEquals(result, 3); 
+ 		String result = Assignment01.classifyTriangle(a, b, c);
+ 		assertEquals(result, "Scalene Right Triangle"); 
+ 	}
+   
+   @Test
+   //Checks if triangle is Isosceles for Double Inputs
+ 	public void checkIsoscelesDouble()
+ 	{
+ 		double a = 7.32;
+ 		double b = 7.32;
+ 		double c = 5.67;
+ 		String result = Assignment01.classifyTriangle(a, b, c);
+ 		assertEquals(result, "Isosceles Triangle"); 
+ 	}
+   
+   @Test
+   //Checks if triangle is Isosceles for Integer Inputs
+ 	public void checkIsoscelesInt()
+ 	{
+ 		int a = 4;
+ 		int b = 4;
+ 		int c = 6;
+ 		String result = Assignment01.classifyTriangle(a, b, c);
+ 		assertEquals(result, "Isosceles Triangle"); 
+ 	}
+   
+   @Test
+   //Checks if triangle is Scalene for Double Inputs
+ 	public void checkScaleneDouble()
+ 	{
+ 		double a = 7.32;
+ 		double b = 8.95;
+ 		double c = 5.67;
+ 		String result = Assignment01.classifyTriangle(a, b, c);
+ 		assertEquals(result, "Scalene Triangle"); 
+ 	}
+   
+   @Test
+   //Checks if triangle is Scalene for Integer Inputs
+ 	public void checkScaleneInt()
+ 	{
+ 		int a = 4;
+ 		int b = 5;
+ 		int c = 6;
+ 		String result = Assignment01.classifyTriangle(a, b, c);
+ 		assertEquals(result, "Scalene Triangle"); 
+ 	}
+   
+   @Test
+   //Checks if Invalid Input
+ 	public void checkInvalidInput01()
+ 	{
+ 		int a = 0;
+ 		int b = 0;
+ 		int c = 0;
+ 		String result = Assignment01.classifyTriangle(a, b, c);
+ 		assertEquals(result, "Invalid Triangle"); 
+ 	}
+   
+   @Test
+   //Checks if Invalid Input
+ 	public void checkInvalidInput02()
+ 	{
+ 		double a = -5.3;
+ 		double b = 1.2;
+ 		double c = -3.4;
+ 		String result = Assignment01.classifyTriangle(a, b, c);
+ 		assertEquals(result, "Invalid Triangle"); 
  	}
 
 }
